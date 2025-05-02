9937dd6539252945977a8b7d07897ace73baec86
DrBrad <brad@bradeagle.com>
2025-05-02 11:13:00 -0600

    Number fix

--- src/variables/bencode_number.rs
+++ src/variables/bencode_number.rs
@@ -53,7 +53,7 @@ macro_rules! impl_decodable_number {
     }
 }
 
-impl_decodable_number!(u8 u16 u32 u64 u128 i8 i16 i32 i64 i128 isize f32 f64);
+impl_decodable_number!(u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize f32 f64);
 
 impl BencodeVariable for BencodeNumber {
 
